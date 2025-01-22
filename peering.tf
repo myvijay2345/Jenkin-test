29799d7c79b989f17e1ab011f718c1df4442630b
vijay <myvijay2345@gmail.com>
2025-01-22 06:13:38 +0000

    v8

--- peering.tf
+++ peering.tf
@@ -3,5 +3,4 @@
     net_link1=data.google_compute_network.ap-its-prod-mgmt-internal-vpc.selflink
     net_link2=data.google_compute_network.ap-its-prod-ic-vpc.selflink
 }*/
-
-net_link1=data.google_compute_network.ap-its-prod-mgmt-internal-vpc.self_link
+#net_link1=data.google_compute_network.ap-its-prod-mgmt-internal-vpc.self_link
