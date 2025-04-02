ceac492bc3a1fcfdb8509e74ad6b9d17caab693f
myvijay2345 <90492600+myvijay2345@users.noreply.github.com>
2025-04-02 15:59:35 +0800

    Update main.tf
    Signed-off-by: myvijay2345 <90492600+myvijay2345@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ module "prod_ic_vpc1" {
       description               = "This is PSC subnet "
       purpose                   = "PRIVATE_SERVICE_CONNECT"
 
-    },
+    }
+''',
     {
       subnet_name               = "new-prod-ic-vpc-sub-1"
       subnet_ip                 = "10.11.3.0/24"
@@ -27,5 +28,6 @@ module "prod_ic_vpc1" {
       purpose                   = "REGIONAL_MANAGED_PROXY"
       role                      = "ACTIVE"
 
-    }]
+    }'''
+]
 }
